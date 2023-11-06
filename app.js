a813fa5883a2e5170db56b07214e00ab169dc14d
Muhammad Asif1163 <bibbasheikh11334@gmail.com>
2023-11-07 00:37:06 +0500

    2 files create

---
+++ app.js
@@ -0,0 +1,48 @@
+
+
+
+var city = prompt("Enter your city name")
+if (city == "Karachi"){
+alert("Welcome to city of lights")
+} 
+
+var gender = prompt("Enter your gender")
+if (gender == "mail"){
+alert("Good Morning Sir")
+}else if(gender == "femail"){
+    alert("Good Morning Maam")
+}
+
+
+var color = prompt("Enter Color")
+if (color == "Red"){
+    alert("Must Stop")
+}else if (color == "Yellow"){
+    alert("Ready to move")
+}else if (color == "Green"){
+    alert("Move now")
+}
+
+var remainingFuel = prompt("Enter fuel")
+if(remainingFuel  <=0.25){
+    alert("Please refill the fuel in your car")
+}
+
+
+var obtainedMarks = prompt("Enter Numbers")
+if(obtainedMarks >=80 ){
+    alert("Grade: A.One \n Remarks: Excellent")
+}
+else if(obtainedMarks >=70 ){
+    alert("Grade: A \n Remarks: Good")
+}
+else if(obtainedMarks >=60 ){
+    alert("Grade: B \n Remarks: You need to improve")
+}
+else if(obtainedMarks  <=59 ){
+    alert("Grade: Fail \n Remarks: Sorry")
+}
+
+
+
+
